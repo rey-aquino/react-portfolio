70f967dd8dc7e9c2504060e8acd50a472bd5bc39
Rey V. Aquino <49036511+includeiostreamsharp@users.noreply.github.com>
2020-06-25 20:20:52 -0400

    Update Preloader.js

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 
 class Preloader extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="preloader">
+          <svg className="circular" viewBox="25 25 50 50">
+            <circle
+              className="path"
+              cx={50}
+              cy={50}
+              r={20}
+              fill="none"
+              strokeWidth={5}
+              strokeMiterlimit={10}
+            />
+          </svg>
+        </div>
+      </div>
+    );
   }
 }
 
