1c86720fbc80be99649a416d5d78545253b0bd48
Rey V. Aquino <49036511+includeiostreamsharp@users.noreply.github.com>
2020-06-25 22:33:15 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,32 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div>
+          <a className="bt-top btn btn-circle btn-lg btn-info" href="#top">
+            <i className="ti-arrow-up" />
+          </a>
+          <footer className="footer row">
+            <div className="fix-width">
+              <div className="row">
+                <span>
+                  <a className="text-white" href="true">
+                    Material Pro
+                  </a>
+                </span>
+                <span className="pull-right">
+                  Design &amp; Developed by
+                  <a className="text-white" href="https://wrappixel.com">
+                    WrapPixel
+                  </a>
+                </span>
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    );
   }
 }
 
