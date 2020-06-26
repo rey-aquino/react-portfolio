9549abef75b224cf759d6af909d4289cbf3b78fa
Rey V. Aquino <49036511+includeiostreamsharp@users.noreply.github.com>
2020-06-25 20:35:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Preloader from './components/Preloader';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Preloader />
+        <Header />
       </div>
     );
   }
