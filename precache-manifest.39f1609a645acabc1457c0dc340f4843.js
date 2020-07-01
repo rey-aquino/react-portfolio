511d8cfd2476f0b27604da901af7395052b8d03d
Rey V. Aquino <49036511+includeiostreamsharp@users.noreply.github.com>
2020-06-30 23:49:02 -0400

    Updates

--- precache-manifest.39f1609a645acabc1457c0dc340f4843.js
+++ precache-manifest.39f1609a645acabc1457c0dc340f4843.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d794d8262617657e604195b6122f03e3",
+    "revision": "995a993d8c4b9fc0c9af62475134d30f",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "6ad3b6bf7c7eef8cd759",
+    "revision": "bf4932031e4fb40c4bb9",
     "url": "/react-portfolio/static/css/main.9adf9338.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.22db53b5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6ad3b6bf7c7eef8cd759",
-    "url": "/react-portfolio/static/js/main.9905f7e3.chunk.js"
+    "revision": "bf4932031e4fb40c4bb9",
+    "url": "/react-portfolio/static/js/main.cb9d9517.chunk.js"
   },
   {
     "revision": "aff33cef9dbb8787d6b3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.899ce512b05a563051b27ef18e42eb7d.js"
+  "/react-portfolio/precache-manifest.39f1609a645acabc1457c0dc340f4843.js"
 );
 
 self.addEventListener('message', (event) => {
